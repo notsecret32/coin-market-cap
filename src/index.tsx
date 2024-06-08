c6eaf02b9a2303a4e02030aff525d5e07bf64e23
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-08 23:32:58 +0300

    feat: added `Tailwind`

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
+import './assets/styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -4,7 +4,7 @@ import { UserList } from '@features/counter/UserList'
 export const HomePage: React.FC = () => {
   return (
     <main>
-      <h1>Coin Market Cap</h1>
+      <h1 className="text-3xl font-bold underline">Coin Market Cap</h1>
       <Counter />
       <UserList />
     </main>
