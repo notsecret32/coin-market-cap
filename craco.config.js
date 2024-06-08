b7629628f997b5098e5a54f75b5f656a5395de18
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-08 22:20:05 +0300

    fix: fixed incorrect alias behavior

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,14 @@
-const path = require('path');
+import CracoAlias from 'craco-alias';
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@api/*': path.resolve(__dirname, 'src/api/'),
-      '@components/*': path.resolve(__dirname, 'src/components/'),
-      '@features/*': path.resolve(__dirname, 'src/features/'),
-      '@hooks/*': path.resolve(__dirname, 'src/hooks/'),
-      '@models/*': path.resolve(__dirname, 'src/models/'),
-      '@state/*': path.resolve(__dirname, 'src/state/'),
-      '@utils/*': path.resolve(__dirname, 'src/utils/'),
-      '@views/*': path.resolve(__dirname, 'src/views/'),
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: './tsconfig.path.json',
+      },
     },
-  },
+  ],
 };
