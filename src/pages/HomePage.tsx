c2a4451e5911abade753870f7b0b145badc36bb0
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-09 13:52:18 +0300

    feat: added `Coin Market Cap Api` support

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
+import { useGetKeyInfoStatusQuery } from 'src/api/keyInfoApi'
 import { Counter, UserList } from 'src/components'
 
 export const HomePage: React.FC = () => {
+  const { data } = useGetKeyInfoStatusQuery()
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Coin Market Cap</h1>
+      {JSON.stringify(data, null, 2)}
       <Counter />
       <UserList />
     </main>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { keyInfoApi } from 'src/api/keyInfoApi'
 import { usersApi } from 'src/api/usersApi'
 import { counterSlice } from './slices/counterSlice'
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
+    [keyInfoApi.reducerPath]: keyInfoApi.reducer,
     counter: counterSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(usersApi.middleware),
+    getDefaultMiddleware()
+      .concat(usersApi.middleware)
+      .concat(keyInfoApi.middleware),
 })
 
 setupListeners(store.dispatch)
