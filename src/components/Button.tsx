1030c1126b2e82d66063aa77a1bf00c6daad169f
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-10 21:46:36 +0300

    feat: added data pagination

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 interface IButtonProps {
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  className?: React.HTMLAttributes<HTMLDivElement> | string
   children: React.ReactNode
 }
 
-export const Button = ({ children }: IButtonProps) => {
+export const Button = ({ children, onClick, className }: IButtonProps) => {
   return (
-    <button className="bg-[#ebecf1] hover:bg-[#c5c5ca] px-3 py-2 rounded-xl font-semibold">
+    <button
+      onClick={onClick}
+      className={`bg-[#ebecf1] hover:bg-[#c5c5ca] px-3 py-2 rounded-xl font-semibold ${className}`}
+    >
       {children}
     </button>
   )
