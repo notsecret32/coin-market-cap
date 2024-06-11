413b19f93aa65ba8d742906befbcb2285c03353d
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-12 00:50:48 +0300

    refactor: unnecessary interfaces have been removed

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,5 @@
 import type { IApiCoin } from './api-coin'
-import type { IApiError } from './api-error'
 import type { IApiResponse } from './api-response'
 import type { IApiStatus } from './api-status'
-import type { IApiTrendingCoins } from './api-trending-coins'
 
-export { IApiCoin, IApiError, IApiResponse, IApiStatus, IApiTrendingCoins }
+export { IApiCoin, IApiResponse, IApiStatus }
