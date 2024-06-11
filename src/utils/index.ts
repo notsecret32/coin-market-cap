34e930bb4110e3e63671662341f3965e6a13cb66
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-12 01:59:34 +0300

    feat: added a new error handling feature

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,13 @@
 import { formatNumber } from './format-number'
 import { getApiErrorMessage } from './get-api-error-message'
 import { getApiStatusResponse } from './get-api-status-response'
+import { handleApiError } from './handle-api-error'
 import { trkBaseQuery } from './rtk-base-query'
 
-export { formatNumber, getApiErrorMessage, getApiStatusResponse, trkBaseQuery }
+export {
+  formatNumber,
+  getApiErrorMessage,
+  getApiStatusResponse,
+  handleApiError,
+  trkBaseQuery,
+}
