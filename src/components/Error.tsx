331bbbb1fea993158a45504d690e142bd3f01287
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-12 02:02:08 +0300

    refactor: an unnecessary function has been removed

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,17 +1,17 @@
 import { SerializedError } from '@reduxjs/toolkit'
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
-import { getApiErrorMessage } from 'src/utils/get-api-error-message'
+import { handleApiError } from 'src/utils'
 
 interface IErrorProps {
   error: FetchBaseQueryError | SerializedError | undefined
 }
 
 export const Error = ({ error }: IErrorProps) => {
+  const { message } = handleApiError({ error })
+
   return (
     <div className="bg-[#ebecf1] text-center py-32 mx-4 lg:mx-32 rounded-xl m-auto">
-      <h1 className="font-semibold text-2xl">
-        {getApiErrorMessage({ error })}
-      </h1>
+      <h1 className="font-semibold text-2xl">{message}</h1>
     </div>
   )
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,6 @@
 import { formatNumber } from './format-number'
-import { getApiErrorMessage } from './get-api-error-message'
 import { getApiStatusResponse } from './get-api-status-response'
 import { handleApiError } from './handle-api-error'
 import { trkBaseQuery } from './rtk-base-query'
 
-export {
-  formatNumber,
-  getApiErrorMessage,
-  getApiStatusResponse,
-  handleApiError,
-  trkBaseQuery,
-}
+export { formatNumber, getApiStatusResponse, handleApiError, trkBaseQuery }
