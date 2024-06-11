4ea4305ebde78b6619d76cdd5dd317e95c13530d
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-12 00:46:49 +0300

    feat: added interface for request status

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import type { IApiCoin } from './api-coin'
 import type { IApiError } from './api-error'
 import type { IApiResponse } from './api-response'
+import type { IApiStatus } from './api-status'
 import type { IApiTrendingCoins } from './api-trending-coins'
 
-export { IApiCoin, IApiError, IApiResponse, IApiTrendingCoins }
+export { IApiCoin, IApiError, IApiResponse, IApiStatus, IApiTrendingCoins }
