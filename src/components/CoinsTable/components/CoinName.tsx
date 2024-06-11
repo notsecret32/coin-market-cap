2b05d76cb0cc9bd9eb38a1ca3d65395a4676a944
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-12 02:29:53 +0300

    refactor: fixed incorrect props for `CoinName`

--- src/components/CoinsTable/components/CoinName.tsx
+++ src/components/CoinsTable/components/CoinName.tsx
@@ -1,22 +1,20 @@
 interface ICoinNameProps {
-  id: number
-  name: string
-  symbol: string
+  id?: number
+  name?: string
+  symbol?: string
 }
 
 export const CoinName = ({ id, name, symbol }: ICoinNameProps) => {
   return (
-    <td className="text-left py-3">
-      <div className="flex flex-row items-center gap-2">
-        <img
-          src={`https://s2.coinmarketcap.com/static/img/coins/32x32/${id}.png`}
-          alt={name}
-          width={32}
-          height={32}
-        />
-        <p>{name}</p>
-        <p className="text-[#58667E]">{symbol}</p>
-      </div>
+    <td className="text-left py-3 flex flex-row items-center gap-2">
+      <img
+        src={`https://s2.coinmarketcap.com/static/img/coins/32x32/${id}.png`}
+        alt={name}
+        width={32}
+        height={32}
+      />
+      <h1>{name}</h1>
+      <p className="text-[#58667E]">{symbol}</p>
     </td>
   )
 }
