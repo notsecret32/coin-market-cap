12263a69ebda908da9c0b4a0a1e0ab406d66d861
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-12 00:46:02 +0300

    feat: added interface for request

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 import type { IApiCoin } from './api-coin'
 import type { IApiError } from './api-error'
-
+import type { IApiResponse } from './api-response'
 import type { IApiTrendingCoins } from './api-trending-coins'
 
-export { IApiCoin, IApiError, IApiTrendingCoins }
+export { IApiCoin, IApiError, IApiResponse, IApiTrendingCoins }
