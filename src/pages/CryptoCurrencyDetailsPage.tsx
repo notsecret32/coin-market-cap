b6614597a3da0cf7ec1f4e6314e6ef588b8dacac
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-13 14:51:06 +0300

    refactor: the commented-out code has been deleted

--- src/pages/CryptoCurrencyDetailsPage.tsx
+++ src/pages/CryptoCurrencyDetailsPage.tsx
@@ -70,58 +70,5 @@ export const CryptoCurrencyDetailsPage = () => {
         />
       </div>
     </Layout>
-    // <main>
-    //   <div className="flex flex-col h-screen sm:w-2/3 xl:w-1/2 mx-4 sm:mx-auto">
-    //     {isLoading ? (
-    //       <Loading />
-    //     ) : !data || error ? (
-    //       <Error error={error} />
-    //     ) : (
-    //       <>
-    //         {/* Header */}
-    //         <CoinDetailsHeader
-    //           id={data.id}
-    //           name={data.name}
-    //           symbol={data.symbol}
-    //           imageUrl={data.imageUrl}
-    //         />
-
-    //         {/* Price */}
-    //         <div className="my-6">
-    //           <h1 className="font-inter font-semibold text-4xl">
-    //             ${data.price?.toFixed(2)}
-    //           </h1>
-    //         </div>
-
-    //         {/* Intervals */}
-    //         <CryptoCurrencyChartIntervals />
-
-    //         {/* Chart */}
-    //         <CryptoCurrencyChart points={data.points} />
-
-    //         {/* Statistics */}
-    //         <div className="flex flex-col mt-auto mb-16 gap-y-4">
-    //           <h2 className="font-inter font-semibold text-sm">Статистика</h2>
-    //           <CoinStatisticItem label="Ранг" value={data.rank} />
-    //           <CoinStatisticItem
-    //             label="Общий запас"
-    //             value={formatNumberWithCommas(data.supply)}
-    //             symbol={data.symbol}
-    //           />
-    //           <CoinStatisticItem
-    //             label="Макс. запас"
-    //             value={formatNumberWithCommas(data.maxSupply)}
-    //             symbol={data.symbol}
-    //           />
-    //           <CoinStatisticItem
-    //             label="Капитализация"
-    //             prefix="$"
-    //             value={formatNumberWithCommas(data.capitalization)}
-    //           />
-    //         </div>
-    //       </>
-    //     )}
-    //   </div>
-    // </main>
   )
 }
