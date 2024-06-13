7a8fd5a27e5d3392a3a0159d1624a6e49f5abe8b
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-13 16:04:59 +0300

    feat: added new package - `react-modal`

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactModal from 'react-modal'
 
 import 'src/assets/styles/index.css'
-
 import App from './App'
 
+ReactModal.setAppElement('#root')
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
