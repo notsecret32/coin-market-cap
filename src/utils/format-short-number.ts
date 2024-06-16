7be9c5d39bc4d670efb9788566f455fa32b5a66e
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-06-16 13:39:40 +0300

    refactor: improved the `formatShortNumber` method

--- src/utils/format-short-number.ts
+++ src/utils/format-short-number.ts
@@ -1,5 +1,8 @@
-export const formatShortNumber = (value?: number): string | undefined => {
-  if (!value || value === undefined) return undefined
+export const formatShortNumber = (
+  value: number | undefined,
+  symbol = '0',
+): string => {
+  if (!value || value === undefined) return symbol
   if (value < 1000) return value.toFixed(2)
   if (value < 1000000) return (value / 1000).toFixed(2) + 'K'
   if (value < 1000000000) return (value / 1000000).toFixed(2) + 'M'
